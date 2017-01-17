8a33d1fe04f8d691a6b8c9c860ee55f2f190199b
Ryan LeViseur <vidkun@users.noreply.github.com>
2017-01-17 10:32:08 -0600

    add basic portfolio custom post type and register a project type taxonomy

--- quicksandcreative-portfolio.php
+++ quicksandcreative-portfolio.php
@@ -4,27 +4,46 @@
 Plugin Name: Quicksand Creative Portfolio
 Plugin URI: https://quicksandcreative.com/
 Description: Provides a custom post type, meta boxes, and page template for a portfolio gallery
-Author: Ryan LeViseur
+Author: Quicksand Creative
 Version: 1.0
 Author URI: https://quicksandcreative.com/
 */
 
-// Modify the excerpt length and 'read more' text
-add_filter('excerpt_length', 'my_excerpt_length');
-
-function my_excerpt_length($length) {
-    return 25;
+if ( function_exists( 'add_theme_support' ) ) { 
+    add_theme_support( 'post-thumbnails' );
+    set_post_thumbnail_size( 280, 210, true ); // Normal post thumbnails
+    add_image_size( 'screen-shot', 720, 540 ); // Full size screen
 }
 
-add_filter('excerpt_more', 'new_excerpt_more');
+add_action( 'init', 'create_portfolio' );
 
-function new_excerpt_more($text){
-    return ' ';
+function create_portfolio() {  
+    $args = array(  
+        'label' => __('Portfolio'),  
+        'singular_label' => __('Project'),  
+        'public' => true,
+        'menu_position' => 3,
+        'show_ui' => true,  
+        'capability_type' => 'post',  
+        'hierarchical' => false,  
+        'rewrite' => true,  
+        'supports' => array('title', 'editor', 'thumbnail', 'custom-fields')  
+       );  
+   
+    register_post_type( 'portfolio' , $args );  
 }
 
-//
-function portfolio_thumbnail_url($pid){
-    $image_id = get_post_thumbnail_id($pid);
-    $image_url = wp_get_attachment_image_src($image_id,'screen-shot');
-    return  $image_url[0];
+add_action( 'init', 'create_project_type_tax' );
+
+function create_project_type_tax() {
+    register_taxonomy(
+        'project-type',
+        'portfolio',
+        array(
+            'label' => __( 'Project Types' ),
+            'singular_label' => __( 'Project Type' ),
+            'rewrite' => true,
+            'hierarchical' => true,
+        )
+    );
 }
